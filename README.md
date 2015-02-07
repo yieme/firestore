32abd736f00d494b237e88132f9a9fb0c7902962
yieme <yieme@cormea.com>
2015-02-07 09:41:24 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# firestore
+# firestore <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="120">
 Firebase implementation of a simple key/value store
+
+# Install
+
+
+```js
+npm install firestore --save
+```
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// firestore - Firebase implementation of a simple key/value store - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var Firestore = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = Firestore // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return Firestore }) // support AMD
+  // else this.Firestore = Firestore // support browser
+})();
