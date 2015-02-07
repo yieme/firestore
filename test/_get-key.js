22b629357231f68de6ed9810344522f78b5e793c
yieme <yieme@cormea.com>
2015-02-07 12:00:33 -0700

    use path instead of child

--- test/_get-key.js
+++ test/_get-key.js
@@ -7,7 +7,7 @@ var _      = require('lodash')
   , exec   = require('child_process').exec
 ;
 describe('example', function() {
-  this.timeout(9000)
+  this.timeout(30000)
   it('Get value', function(done) {
   exec('node examples/get-key.js', function (error, stdout, stderr) {
     if (error) throw error
